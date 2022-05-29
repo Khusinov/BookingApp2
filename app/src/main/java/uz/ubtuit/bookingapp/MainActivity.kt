59668f49d6d9500952fea6dbe8fded7b9d8cd67a
Khusinov <shaxriyorxusinov2020@gmail.com>
2022-05-29 15:21:38 +0500

    initial commit

---
+++ app/src/main/java/uz/ubtuit/bookingapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.ubtuit.bookingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
