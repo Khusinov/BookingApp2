5db9db31ca63ddd3b08e9081dc0c584d03efd2f0
ACER <alibekadamboyev@gmail.com>
2022-05-30 15:56:20 +0500

    ui changed

--- app/src/main/java/uz/ubtuit/bookingapp/MainActivity.kt
+++ app/src/main/java/uz/ubtuit/bookingapp/MainActivity.kt
@@ -2,10 +2,20 @@ package uz.ubtuit.bookingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import uz.ubtuit.bookingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
---
+++ app/src/main/java/uz/ubtuit/bookingapp/UI/Adapter.kt
@@ -0,0 +1,41 @@
+package uz.ubtuit.bookingapp.UI
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import uz.ubtuit.bookingapp.R
+import uz.ubtuit.bookingapp.databinding.ItemBinding
+import uz.ubtuit.bookingapp.model.Services
+
+class ServicesAdapter(): RecyclerView.Adapter<ServicesAdapter.ServicesHolder>() {
+    private val list = mutableListOf<Services>()
+
+    fun submitList(list: List<Services>, listOf: List<Services>, listOf1: List<Services>) {
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    class ServicesHolder(item: View): RecyclerView.ViewHolder(item) {
+        val binding = ItemBinding.bind(item)
+        fun bind(services: Services) = binding.apply {
+            specialistIV.setImageResource(services.imageId)
+            spesialitsName.setText(services.name)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServicesHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item, parent, false)
+        return ServicesHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ServicesHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
